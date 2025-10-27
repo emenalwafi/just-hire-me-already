cee5bc7670b5fad7a6816f78d0dfaa2a224215dc
nashih <nashih@definite.co.id>
2025-10-27 18:30:04 +0700

    [HireMe][GIT][CI]: fIX Error jest test the playwright config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,17 @@
-import nextJest from 'next/jest.js'
- 
+import nextJest from "next/jest.js";
+
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
- 
+  dir: "./",
+});
+
 // Add any custom config to be passed to Jest
 const config = {
-  coverageProvider: 'v8',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-}
- 
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testPathIgnorePatterns: ["<rootDir>/e2e/"],
+};
+
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
+export default createJestConfig(config);
